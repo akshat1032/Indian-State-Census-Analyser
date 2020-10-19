700333d8a8798ed9c2c2a0be3b26e84e22804e98
akshat1032 <akshat.30oct@gmail.com>
2020-10-19 17:25:58 +0530

    Added : Welcome message display added

---
+++ src/main/java/com/capgemini/indianstatecensusprogram/CSVStateCensus.java
@@ -0,0 +1,14 @@
+package com.capgemini.indianstatecensusprogram;
+
+import java.util.logging.Logger;
+
+public class CSVStateCensus 
+{
+    public static void main( String[] args )
+    {	
+    	Logger log = Logger.getLogger(CSVStateCensus.class.getName());
+    	
+//    	Welcome message added
+        log.info("Welcome to Indian State Census Analyser Program!" );
+    }
+}
